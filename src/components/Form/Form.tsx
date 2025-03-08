c0fe605ec24dff05934cd90757e8c5b2d3a59c40
DavidFrontendDev <daviddevbackend@gmail.com>
2025-03-08 15:31:36 +0100

    Error de form definido

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { countries } from "../../data/countries";
 import styles from "./Form.module.css";
 import { SearchType } from "../../types";
+import Alert from "../Alert/Alert";
 
 function Form() {
   const [search, setSearch] = useState<SearchType>({
     city: "",
     country: "",
   });
+  const [alert, setAlert] = useState("");
 
   const handleChange = (
     e:
@@ -20,8 +22,17 @@ function Form() {
     });
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (Object.values(search).includes("")) {
+      setAlert("Todos los campos son obligatorios");
+      return;
+    }
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      {alert && <Alert>{alert}</Alert>}
       <div className={styles.field}>
         <label htmlFor="city">Ciudad:</label>
         <input
